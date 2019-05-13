a9cded8482bb4fe618544361954a6639391cf9ce
khorea <horea.frinco@gmail.com>
2019-05-14 00:56:49 +0300

    Initial commit

---
+++ 2011_nat_Discuri/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _2011_nat_Discuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.raza_textb = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.exit_Button = new System.Windows.Forms.Button();
+            this.newImage_Button = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // raza_textb
+            // 
+            this.raza_textb.Location = new System.Drawing.Point(125, 14);
+            this.raza_textb.Name = "raza_textb";
+            this.raza_textb.Size = new System.Drawing.Size(47, 19);
+            this.raza_textb.TabIndex = 0;
+            this.raza_textb.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(210, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Raza discului:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Culoarea:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox2.Location = new System.Drawing.Point(99, 137);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 38);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(151, 23);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Discuri nesuprapuse";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(12, 90);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 23);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Discuri";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // exit_Button
+            // 
+            this.exit_Button.Location = new System.Drawing.Point(12, 383);
+            this.exit_Button.Name = "exit_Button";
+            this.exit_Button.Size = new System.Drawing.Size(192, 29);
+            this.exit_Button.TabIndex = 7;
+            this.exit_Button.Text = "Iesire";
+            this.exit_Button.UseVisualStyleBackColor = true;
+            this.exit_Button.Click += new System.EventHandler(this.exit_Button_Click);
+            // 
+            // newImage_Button
+            // 
+            this.newImage_Button.Location = new System.Drawing.Point(12, 191);
+            this.newImage_Button.Name = "newImage_Button";
+            this.newImage_Button.Size = new System.Drawing.Size(192, 29);
+            this.newImage_Button.TabIndex = 8;
+            this.newImage_Button.Text = "Imagine noua";
+            this.newImage_Button.UseVisualStyleBackColor = true;
+            this.newImage_Button.Click += new System.EventHandler(this.newImage_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 424);
+            this.Controls.Add(this.newImage_Button);
+            this.Controls.Add(this.exit_Button);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.raza_textb);
+            this.Name = "Form1";
+            this.Text = "Desenare Discuri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox raza_textb;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button exit_Button;
+        private System.Windows.Forms.Button newImage_Button;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ 2011_nat_Discuri/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _2011_nat_Discuri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        SolidBrush b;
+
+        Bitmap bmp = new Bitmap(100,100);
+        GraphicsPath gPath;
+
+        Random rand = new Random();
+
+        bool suprapuse = false;
+        bool sesuprapune = false;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(raza_textb.Text != "")
+            {
+                try
+                {
+                    sesuprapune = false;
+
+                    g = Graphics.FromImage(bmp);
+                    b = new SolidBrush(pictureBox2.BackColor);            
+
+                    Point point = pictureBox1.PointToClient(Cursor.Position);
+
+                    Rectangle rect = new Rectangle(point.X - Convert.ToInt32(raza_textb.Text) / 2, point.Y - Convert.ToInt32(raza_textb.Text) / 2,
+                                                   Convert.ToInt32(raza_textb.Text), Convert.ToInt32(raza_textb.Text));                 
+
+                    if (suprapuse)
+                    {
+                        gPath.AddEllipse(rect);
+                        g.FillEllipse(b, rect);
+
+                        pictureBox1.Image = bmp;
+                    }
+                    else
+                    {
+                        GraphicsPath gPath2 = new GraphicsPath();
+                        gPath2.AddEllipse(rect);
+
+                        Region r1 = new Region(gPath);
+
+                        r1.Intersect(gPath2);
+
+                        if(!r1.IsEmpty(g))
+                        {
+                            sesuprapune = true;
+                        }
+                        if (!sesuprapune)
+                        {
+                            gPath.AddEllipse(rect);
+                            g.FillEllipse(b, rect);
+
+                            pictureBox1.Image = bmp;
+                        }                 
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("!_!");
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gPath = new GraphicsPath();
+
+            pictureBox1.BackColor = Color.White;
+
+            radioButton1.Checked = true;
+        }
+
+        private void exit_Button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                suprapuse = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                suprapuse = true;
+            }
+        }
+
+        private void newImage_Button_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = bmp;
+
+            gPath = new GraphicsPath();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog(); ;
+
+            if (dr == DialogResult.OK) pictureBox2.BackColor = colorDialog1.Color;        
+        }
+    }
+}
